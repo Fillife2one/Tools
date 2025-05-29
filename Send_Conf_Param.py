4175785df1b1cf1e99e4c243fed20e28534a7fa8
Fillife2one <Fillife.one@ya.ru>
2025-05-29 17:14:57 +0300

    Initial commit

--- Send_Conf_Param.py
+++ Send_Conf_Param.py
@@ -4,10 +4,10 @@ import message_pb2
 from kafka import KafkaProducer
 import ssl
 
-load_dotenv()
-KAFKA_USERNAME = os.environ['KAFKA_USERNAME']
-KAFKA_PASSWORD = os.environ['KAFKA_PASSWORD']
-BROCKER_DEV_TEST=os.environ['BROCKER_DEV_TEST']
+#load_dotenv()
+#KAFKA_USERNAME = os.environ['KAFKA_USERNAME']
+#KAFKA_PASSWORD = os.environ['KAFKA_PASSWORD']
+#BROKER_DEV_TEST=os.environ['BROCKER_DEV_TEST']
 
 # Заполняем сообщение
 message = message_pb2.SetNewCustomerConfigurationResponse()
@@ -23,11 +23,11 @@ ssl_context = ssl.create_default_context()
 ssl_context.load_verify_locations(r'C:\Users\user\PycharmProjects\ConfState\resourse\CA.pem')
 
 producer = KafkaProducer(
-    bootstrap_servers=BROCKER_DEV_TEST, # # Получить в Vault
+    bootstrap_servers=['BROKER'],  # Получить в Vault
     security_protocol='SASL_SSL',
     sasl_mechanism='SCRAM-SHA-512',
-    sasl_plain_username=KAFKA_USERNAME, # Получить в Vault
-    sasl_plain_password=KAFKA_PASSWORD, # Получить в Vault
+    sasl_plain_username='login', # Получить в Vault
+    sasl_plain_password='password', # Получить в Vault
     ssl_context=ssl_context  
 )
 
